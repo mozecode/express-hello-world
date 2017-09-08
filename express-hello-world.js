93eaf935cf829e1bc222c234c02aa7352bada439
Greta Moseley <greta.moseley@gmail.com>
2017-09-07 21:40:41 -0500

    "express-hello-world exercise completed"

---
+++ express-hello-world.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express();
+let date = new Date();
+let newDate = date.toISOString();
+var port = process.env.PORT || 8080;//set environment variable for port specified on command line
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/time', (req, res)=>{
+    res.send(`The date and time are: ${newDate}`);
+});
+
+console.log("port specified?", port);
+app.listen(port, ()=>{
+    console.log("listening on port specified.")
+});
